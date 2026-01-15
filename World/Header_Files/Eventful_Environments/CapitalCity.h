ffb525b4e14d74adbde6d8f41524b569060aed24
ImamM05 <imammishkat@gmail.com>
2026-01-15 15:32:28 -0500

    working on CapitalCity

--- World/Header_Files/Eventful_Environments/CapitalCity.h
+++ World/Header_Files/Eventful_Environments/CapitalCity.h
@@ -1,15 +1,15 @@
 #ifndef CAPITALCITY_H
 #define CAPITALCITY_H
 #include "../City.h"
+#include "../../../Utils/Constants/CapitalCityConstants.h"
 
 namespace World {
     class CapitalCity : public City {
         private:
-
+            const int intent[Constants_CapitalCity::MAX_INTENT];
         public:
             void visitLandMark() override;
             void goShopping() override;
-            
 
     };
 }
